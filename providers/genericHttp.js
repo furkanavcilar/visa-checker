58f9cda1c4acfe69527db8a2e024494ee440a467
Furkan <94824708+furkanavcilar@users.noreply.github.com>
2025-11-03 17:43:55 +0300

    Refactor genericHttp.js to use ESM and exports
    Updated genericHttp.js to use ESM syntax and export functions.

--- providers/genericHttp.js
+++ providers/genericHttp.js
@@ -1,7 +1,7 @@
 // providers/genericHttp.js
-// Genel HTTP istek fonksiyonu - axios kullanır
+// Genel HTTP istek fonksiyonu - axios kullanır (ESM versiyon)
 
-const axios = require('axios');
+import axios from 'axios';
 
 /**
  * Basit HTTP GET isteği yapar ve sonucu döner
@@ -9,7 +9,7 @@ const axios = require('axios');
  * @param {object} headers - Ek header bilgileri (opsiyonel)
  * @returns {Promise<any>}
  */
-async function fetchPage(url, headers = {}) {
+export async function fetchPage(url, headers = {}) {
   try {
     const response = await axios.get(url, { headers, timeout: 10000 });
     return response.data;
@@ -25,13 +25,8 @@ async function fetchPage(url, headers = {}) {
  * @param {RegExp|string} pattern - Aranacak metin veya regex
  * @returns {boolean}
  */
-function containsPattern(html, pattern) {
+export function containsPattern(html, pattern) {
   if (!html) return false;
   const regex = pattern instanceof RegExp ? pattern : new RegExp(pattern, 'i');
   return regex.test(html);
 }
-
-module.exports = {
-  fetchPage,
-  containsPattern,
-};
